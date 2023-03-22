6c2230dff6cdddf90b98017d3c179db6e88e5c7d
crow625 <ethanscrow@gmail.com>
2023-03-21 18:24:31 -0700

    Echo no longer relies on server functionality

--- src/App.js
+++ src/App.js
@@ -8,21 +8,12 @@ const App = () => {
 
     const submitForm = async (e) => {
         e.preventDefault();
-        const submitResponse = await fetch(`api/example`, {
-            method: 'get',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-        });
-
-        const r = await submitResponse.json(); // Don't forget .json() lol
-        setReply(r.message);
+        
+        setReply(`${message} from the backend!`);
     }
 
     return (
         <>
-            <h1>Whoops</h1>
-            <p>Turns out, you can't run a server on github pages. Time to find a different place.</p>
             <Form onSubmit={submitForm}>
                 <Input type="text" id="message" value={message} onChange={(e) => {setMessage(e.target.value)}} />
 
