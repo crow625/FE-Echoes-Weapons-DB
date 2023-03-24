b452ea4a1e3f758ccb632186baac835cb0dbf514
crow625 <ethanscrow@gmail.com>
2023-03-23 21:28:38 -0700

    Weapon selector in App

--- src/App.js
+++ src/App.js
@@ -1,52 +1,40 @@
 import { useState, useEffect } from 'react';
-import { Button, Form, Input } from 'reactstrap';
+import { Button, Form, FormGroup, Input } from 'reactstrap';
 import swords from './json/swords.json';
-import { WeaponSelector } from './Components/WeaponSelector';
 
 const App = () => {
 
-    const [message, setMessage] = useState('');
-    const [reply, setReply] = useState('');
-    const [weapon, setWeapon] = useState({});
+    const [weapon, setWeapon] = useState("");
 
-    // useEffect(() => {
-    //     console.log(swords);
-    // }, [])
-
-    const onWeaponSelectHandler = (val) => {
-        console.log(val);
-        setWeapon(val);
-        // console.log(weapon.name);
-    }
+    useEffect(() => {
+        console.log(swords);
+    }, []);
 
     const submitForm = async (e) => {
         e.preventDefault();
-        const submitResponse = await fetch(`api/example`, {
-            method: 'get',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-        });
-
-        const r = await submitResponse.json(); // Don't forget .json() lol
-        setReply(r.message);
+        console.log(weapon);
     }
 
     return (
         <>
             <Form onSubmit={submitForm}>
-                <WeaponSelector 
-                    weapons={swords} 
-                    onWeaponSelect={onWeaponSelectHandler}
-                    weapon={weapon}
-                />
-                {/* <Input type="text" id="message" value={message} onChange={(e) => {setMessage(e.target.value)}} /> */}
-
-                {/* <Button type="submit">Send</Button> */}
+                <FormGroup>
+                    <Input 
+                        type="select" 
+                        value={weapon} 
+                        onChange={(e) => {setWeapon(e.target.value)}}
+                    >
+                        <option value="" hidden>Select a Weapon</option>
+                        {swords.map((item, i) => {
+                            return (
+                                <option value={item.name} key={i}>{item.name}</option>
+                            )
+                        })}
+                    </Input>
+                </FormGroup>
             </Form>
             
-            {/* {reply && <p>{reply}</p>} */}
-            {weapon && <p>{weapon.name}</p>}
+            {weapon && <p>{`${weapon}: ${swords.find(s => s.name === weapon).mt} mt`}</p>}
         </>
     )
 }
